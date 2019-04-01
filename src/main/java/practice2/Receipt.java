ff5fdc7ed816cd643d78b93615c06dfa09a91b92
xion <313183373@qq.com>
2019-04-01 12:37:18 +0800

    extract method to calc total discount, and use stream to make code more elegant

--- src/main/java/practice2/Receipt.java
+++ src/main/java/practice2/Receipt.java
@@ -13,42 +13,34 @@ public class Receipt {
     private BigDecimal tax;
 
     public double CalculateGrandTotal(List<Product> products, List<OrderItem> items) {
-        BigDecimal subTotal = calculateSubtotal(products, items);
-
-        for (Product product : products) {
-            OrderItem curItem = findOrderItemByProduct(items, product);
-
-            BigDecimal reducedPrice = product.getPrice()
-                    .multiply(product.getDiscountRate())
-                    .multiply(new BigDecimal(curItem.getCount()));
-
-            subTotal = subTotal.subtract(reducedPrice);
-        }
+        BigDecimal subTotal = calculateSubtotal(products, items).subtract(calculateDiscountTotal(products, items));
         BigDecimal taxTotal = subTotal.multiply(tax);
         BigDecimal grandTotal = subTotal.add(taxTotal);
 
         return grandTotal.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
     }
 
+    private BigDecimal calculateDiscountTotal(List<Product> products, List<OrderItem> items) {
+        return products.stream().map(product -> {
+            OrderItem orderItem = findOrderItemByProduct(items, product);
+            return product.getPrice()
+                    .multiply(new BigDecimal(orderItem.getCount()))
+                    .multiply(product.getDiscountRate());
+        }).reduce(BigDecimal::add).orElse(null);
+    }
+
 
     private OrderItem findOrderItemByProduct(List<OrderItem> items, Product product) {
-        OrderItem curItem = null;
-        for (OrderItem item : items) {
-            if (item.getCode() == product.getCode()) {
-                curItem = item;
-                break;
-            }
-        }
-        return curItem;
+        return items.stream()
+                .filter(orderItem -> orderItem.getCode() == product.getCode())
+                .findFirst().orElse(null);
     }
 
     private BigDecimal calculateSubtotal(List<Product> products, List<OrderItem> items) {
-        BigDecimal subTotal = new BigDecimal(0);
-        for (Product product : products) {
-            OrderItem item = findOrderItemByProduct(items, product);
-            BigDecimal itemTotal = product.getPrice().multiply(new BigDecimal(item.getCount()));
-            subTotal = subTotal.add(itemTotal);
-        }
-        return subTotal;
+        return products.stream().map(product -> {
+            OrderItem orderItem = findOrderItemByProduct(items, product);
+            return product.getPrice().multiply(new BigDecimal(orderItem.getCount()));
+        }).reduce(BigDecimal::add).orElse(null);
     }
+
 }
